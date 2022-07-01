297aa8adcc3467a65bf729c0c8ade194dfe9d1d5
VXLTIT <danil050706@gmail.com>
2022-07-01 17:25:50 +0500

    changeTheme

--- Calculator.js
+++ Calculator.js
@@ -1,21 +1,38 @@
 
-// DarkTheme
+// Button changeTheme
 
-    let btn, htmlDoc, header1;
-    btn = document.querySelector( 'button' );
-    header1 = document.querySelector( 'h1' );
-    htmlDoc = document.querySelector( 'html' );
-    btn.addEventListener( 'click', darkTheme );
+    let btn, 
+    htmlDoc, 
+    header1,
+    theme,
+    dark, light;
 
-    function darkTheme() {
+    btn = document.querySelector('button');
+    header1 = document.querySelector('h1');
+    htmlDoc = document.querySelector('html');
+    btn.addEventListener('click', changeThemeDark)
+
+
+    function changeThemeDark() {
         htmlDoc.style.backgroundColor = '#252424';
         btn.style.color = '#252424';
-        header1.style.color = '252424';
+        header1.style.color = '#252424';
+        dark = htmlDoc.style.backgroundColor
+    }
+
+    function changeThemeWhite() {
+        htmlDoc.style.backgroundColor = 'white';
+        btn.style.color = 'white';
+        header1.style.color = 'white';
     }
 
 // Calculator display
 
     let para, div, bod;
-    para = document.createElement( 'p' );
-    div = document.querySelector( 'div' );
-    div.appendChild( para );
+    para = document.createElement('p');
+    div = document.querySelector('div');
+    div.appendChild(para);
+
+// Calculator buttons
+
+    
