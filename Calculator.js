691f5425f041826e6da6c92c1f964f4061d58ddd
VXLTIT <danil050706@gmail.com>
2022-07-02 17:11:47 +0500

    theme switching

--- Calculator.js
+++ Calculator.js
@@ -4,28 +4,40 @@
     let btn, 
     htmlDoc, 
     header1,
-    theme,
-    dark, light;
+    theme;
 
+    theme = 'white'
     btn = document.querySelector('button');
     header1 = document.querySelector('h1');
     htmlDoc = document.querySelector('html');
-    btn.addEventListener('click', changeThemeDark)
+    btn.addEventListener('click', changeTheme);
 
 
+    function changeTheme() {
+        
+        if (theme == 'white') {
+            changeDark();
+        } else {
+            changeWhite();
+        }
+    }
+
+    let changeDark = changeThemeDark;
     function changeThemeDark() {
+        theme = 'dark'
         htmlDoc.style.backgroundColor = '#252424';
         btn.style.color = '#252424';
         header1.style.color = '#252424';
-        dark = htmlDoc.style.backgroundColor
     }
 
+    let changeWhite = changeThemeWhite;
     function changeThemeWhite() {
+        theme = 'white';
         htmlDoc.style.backgroundColor = 'white';
         btn.style.color = 'white';
         header1.style.color = 'white';
-    }
 
+    }
 // Calculator display
 
     let para, div, bod;
