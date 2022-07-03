53cd8f4017ebaacccc2f0a4863fb5b1cda66913e
VXLTIT <danil050706@gmail.com>
2022-07-03 13:20:06 +0500

    edits and workink buttons(0, 1, 2, +, C)

--- Calculator.js
+++ Calculator.js
@@ -47,4 +47,66 @@
 
 // Calculator buttons
 
+    let zero, one, two, three,
+        four, five, six, seven, 
+        eight, nine;
+
+    let minus, plus, clean, sign,
+        remainder, root, degree, 
+        division, multiply, equals;
+
+    zero = document.querySelector('#zero');
+    zero.addEventListener('click', zeroFunction);
+
+    one = document.querySelector('#one');
+    one.addEventListener('click', oneFunction);
+
+    two = document.querySelector('#two');
+    two.addEventListener('click', twoFunction);
+
+    plus = document.querySelector('#plus');
+    plus.addEventListener('click', plusFunction);
     
+
+    clean = document.querySelector('.clean');
+    clean.addEventListener('click', cleanFunction);
+
+
+// Function for calculator buttons
+
+    function zeroFunction() {
+        let symbol = '0',
+            num = '';
+
+        num += symbol;
+        para.textContent += num;
+    }
+
+    function oneFunction() {
+        let symbol = '1',
+            num = '';
+
+        num += symbol;
+        para.textContent += num;
+    }
+
+    function twoFunction() {
+        let symbol = '2',
+            num = '';
+
+            num += symbol;
+            para.textContent += num;
+    }
+
+    function plusFunction() {
+        let symbol = '+'
+            num = '';
+
+            num = num + ' ' + ' ' + symbol;
+            para.textContent += num;
+    }
+
+    function cleanFunction() {
+        para.textContent = ''
+    }
+
